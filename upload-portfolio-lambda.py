d86c23b0f050c1bdcf3949716d7eea5389b75737
Peter Villiers <pvillier@amazon.com>
2018-09-25 12:09:58 -0400

    Added sns messaging

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -5,35 +5,47 @@ import zipfile
 import time
 import mimetypes
 
+
+
 def lambda_handler(event, context):
+    sns = boto3.resource('sns')
+    topic = sns.Topic("arn:aws:sns:us-east-1:370033808472:acloudguruDeployPortfolioTopic")
+
+    try:
+        s3 = boto3.resource('s3')
+        portfolio_bucket = s3.Bucket('pvillier-aws-demo-space')
+        build_bucket = s3.Bucket('pvillier-acloudguru-portfoliobuild')
+        
+        
+        portfolio_zip = io.BytesIO()
+        build_bucket.download_fileobj('acloudguru-portfoliobuild.zip', portfolio_zip)
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, "acloudguru-portfolio-react/" + nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+        
+        
+        cloudfront = boto3.client('cloudfront')
+        cloudfront.create_invalidation(
+            DistributionId='E1KFVD4DUCYA8E',
+            InvalidationBatch={
+                'Paths':  {
+                    'Quantity': 1,
+                    'Items': [
+                        '/acloudguru-portfolio-react/*'
+                    ]
+                },
+                'CallerReference': str(time.time())
+            }
+        )
     
-    
-    s3 = boto3.resource('s3')
-    portfolio_bucket = s3.Bucket('pvillier-aws-demo-space')
-    build_bucket = s3.Bucket('pvillier-acloudguru-portfoliobuild')
-    
-    
-    portfolio_zip = io.BytesIO()
-    build_bucket.download_fileobj('acloudguru-portfoliobuild.zip', portfolio_zip)
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, "acloudguru-portfolio-react/" + nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-    
-    
-    cloudfront = boto3.client('cloudfront')
-    cloudfront.create_invalidation(
-        DistributionId='E1KFVD4DUCYA8E',
-        InvalidationBatch={
-            'Paths':  {
-                'Quantity': 1,
-                'Items': [
-                    '/acloudguru-portfolio-react/*'
-                ]
-            },
-            'CallerReference': str(time.time())
-        }
-    )
+        print("Job Done!")
+        topic.publish(Subject="Portfolio Deploy", Message="acloudguru portfolio was deployed succesfully")
+    except:
+        topic.publish(Subject="Portfolio Deploy Failed", Message="acloudguru portfolio was NOT deployed succesfully")
+        raise
+        
+
 
     # TODO implement
     return {
